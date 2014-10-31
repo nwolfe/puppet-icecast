03ddf8f07a70a27807b20fedbadb861688104c87
Nate Wolfe <nwolfe@puppetlabs.com>
2014-10-31 16:14:24 -0700

    Try again

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,15 +1,16 @@
 class icecast::config {
-  include icecast::install
 
   file { '/etc/icecast2/icecast.xml':
     ensure  => present,
     owner   => 'icecast2',
     group   => 'icecast',
     mode    => 660,
-    content => template("${module_name}/icecast.xml.erb")
+    content => template("${module_name}/icecast.xml.erb"),
+    require => Class['icecast::install']
   }
 
   file { '/etc/default/icecast2':
-    source => "puppet:///modules/${module_name}/icecast2"
+    source => "puppet:///modules/${module_name}/icecast2",
+    require => Class['icecast::install']
   }
 }
