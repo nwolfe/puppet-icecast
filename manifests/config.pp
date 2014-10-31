2b151af09e256457a37a15f7064a90ae1a217bd7
Nate Wolfe <nwolfe@puppetlabs.com>
2014-10-31 15:55:59 -0700

    Manage defaults file

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,10 +1,13 @@
-class icecast::config {
-  file { "/etc/icecast2/icecast.xml":
-    ensure => present,
-    owner  => "icecast2",
-    group  => "icecast",
-    mode   => 660,
-    require => Class["icecast::install"],
+class icecast::config -> icecast::install {
+  file { '/etc/icecast2/icecast.xml':
+    ensure  => present,
+    owner   => 'icecast2',
+    group   => 'icecast',
+    mode    => 660,
     content => template("${module_name}/icecast.xml.erb")
+  }
+
+  file { '/etc/default/icecast2':
+    source => "puppet:///modules/${module_name}/icecast2"
   }
 }
